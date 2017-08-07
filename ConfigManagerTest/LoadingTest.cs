f4bc7958ced9925e840ce98b5d9aae5cb2fd6266
Mikhail158 <ermichin1999@gmail.com>
2017-08-07 14:10:34 +0300

    Started development of Unit tests
    Added test for standard structure of config
    Added test for structure of empty config
    Added test for structure of config with only 1 key and value
    Adde tools for getting and testing private data of ConfigValue

---
+++ ConfigManagerTest/LoadingTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConfigManager;
+using System;
+
+namespace ConfigManagerTest
+{
+    [TestClass]
+    public class LoadingTest
+    {
+        [TestMethod]
+        public void TestLoadEmpty()
+        {
+            var config = LoadValidConfig("");
+
+            Assert.AreEqual(0, config.GetKeys().Length, "Config should have no enclosed values by default");
+        }
+
+        [TestMethod]
+        public void TestLoadValue()
+        {
+            var config = LoadValidConfig("key value");
+
+            Assert.AreEqual(1, config.GetKeys().Length, "1 value should be loaded");
+
+            ConfigValue value = config.Get(config.GetKeys()[0]);
+            Assert.IsNotNull(value, "Should get existing key");
+            Assert.AreEqual("value", ConfigTestTools.GetData(value), "Should have data after key");
+        }
+
+        private static ConfigValue LoadValidConfig(string data)
+        {
+            ConfigValue config = Config.Load(data);
+
+            Assert.IsNotNull(config, "Empty config should be valid");
+            Assert.IsNull(ConfigTestTools.GetData(config), "Config top level data should be null");
+            Assert.IsNotNull(config.AsConfigList(), "Config should always have parsed values list");
+            Assert.AreEqual(0, config.AsConfigList().Count, "Config should have no data values");
+
+            return config;
+        }
+    }
+}
---
+++ ConfigManagerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConfigManagerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConfigManagerTest")]
+[assembly: AssemblyCopyright("Copyright © Ermishin Michael 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36aaf0fd-7159-4b9e-83f3-e68e493ecbe9")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
